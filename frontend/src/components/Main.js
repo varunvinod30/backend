1129572238ce239568f3e212f8ec6ca55d2bb9e6
varunvinod30 <varunvinod30@gmail.com>
2025-03-25 22:21:09 +0530

    test5

---
+++ frontend/src/components/Main.js
@@ -0,0 +1,16 @@
+import Gallery from './Gallery';
+import Awards from './Awards'; 
+import Profile from './Profile';
+
+function Main() {
+  return (
+    <>
+      <main>
+        <Profile />
+        <Gallery />
+        <Awards />
+      </main>
+    </>
+  );
+}
+export default Main;
